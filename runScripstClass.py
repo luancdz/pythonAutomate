83aa831c663b6f5a74b180620fae45c9136d103b
Luan Cardozo <luanyolo@gmail.com>
2021-08-27 15:56:35 -0300

    Movimentação dos scritps para a classe runScriptsClass

--- runScripstClass.py
+++ runScripstClass.py
@@ -10,7 +10,7 @@ class RunScripts(object):
         
         self.SapGuiSession = conn.session
 
-    def getOVfromProd(self, ovOrig):
+    def getSOfromProd(self, ovOrig):
 
         try:
 
@@ -39,6 +39,219 @@ class RunScripts(object):
             
             raise print(sys.exc_info()[0])
 
+    def createSOByRef(self, tpSO, SOProd):
+        try:
+
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nva01"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtVBAK-AUART").text = tpSO
+            self.SapGuiSession.findById("wnd[0]").sendVKey(8)
+            self.SapGuiSession.findById("wnd[1]/usr/tabsMYTABSTRIP/tabpRAUF").select()
+            self.SapGuiSession.findById("wnd[1]/usr/tabsMYTABSTRIP/tabpRAUF/ssubSUB1:SAPLV45C:0301/ctxtLV45C-VBELN").text = SOProd
+            self.SapGuiSession.findById("wnd[1]/usr/tabsMYTABSTRIP/tabpRAUF/ssubSUB1:SAPLV45C:0301/ctxtLV45C-VBELN").caretPosition = 10
+            self.SapGuiSession.findById("wnd[1]").sendVKey(5)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/cmbVBAK-LIFSK").key = " "
+            self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/cmbVBAK-LIFSK").setFocus
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/btn[11]").press
+            self.SapGuiSession.findById("wnd[0]").sendVKey(11)
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nva02"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            return  str(self.SapGuiSession.findById("wnd[0]/usr/ctxtVBAK-VBELN").text)
+        
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def getItmNum(self, sORef, shipPoint):
+        try:
+            
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nzse16n"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtGD-TAB").text = "vbap"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").text = str(sORef)
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").setFocus()
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").caretPosition = 10
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/txtGS_SELFIELDS-FIELDNAME[6,1]").setFocus()
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/txtGS_SELFIELDS-FIELDNAME[6,1]").caretPosition = 5
+            
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/btn[71]").press()
+            self.SapGuiSession.findById("wnd[1]/usr/sub:SAPLSPO4:0300/txtSVALD-VALUE[0,21]").text = "VSTEL"
+            self.SapGuiSession.findById("wnd[1]/usr/sub:SAPLSPO4:0300/txtSVALD-VALUE[0,21]").caretPosition = 5
+            self.SapGuiSession.findById("wnd[1]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,0]").text = str(shipPoint)
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,0]").setFocus()
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,0]").caretPosition = 4
+            self.SapGuiSession.findById("wnd[0]/tbar[1]/btn[8]").press()
+            
+            self.SapGuiSession.findById("wnd[0]/usr/txtGD-NUMBER").setFocus()
+            self.SapGuiSession.findById("wnd[0]/usr/txtGD-NUMBER").caretPosition = 0
+            return str(self.SapGuiSession.findById("wnd[0]/usr/txtGD-NUMBER").text)
+        
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def getStoredLoc(self, shipPoint, sORef, index):
+        try:
+            
+            #Recupera storedlocation
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nvl01n"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtLIKP-VSTEL").text = str(shipPoint)
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtLV50C-VBELN").text = str(sORef)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02").select()
+
+            stLocation = str(self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/ctxtLIPS-LGORT[3," + str(index) +"]").text)
 
 
+            self.SapGuiSession.findById("wnd[0]").sendVKey(11)
+            self.SapGuiSession.findById("wnd[1]").sendVKey(0)      
 
+            return stLocation
+            
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def getDelivery(self):
+        try:
+            
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nvl02n"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+
+            return str(self.SapGuiSession.findById("wnd[0]/usr/ctxtLIKP-VBELN").text)
+                    
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def setStockAndSerNumber(self, sOref, plant, nrItem, matnr, qtdItem, stLocation):
+        try:
+            
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nmb1c"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtRM07M-BWARTWA").text = "561"
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtRM07M-SOBKZ").text = "E"
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtRM07M-WERKS").text = plant
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtRM07M-GRUND").setFocus
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtRM07M-GRUND").caretPosition = 0
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/subBLOCK1:SAPMM07M:2423/ctxtMSEGK-MAT_KDAUF").text = sOref
+            self.SapGuiSession.findById("wnd[0]/usr/subBLOCK1:SAPMM07M:2423/txtMSEGK-MAT_KDPOS").text = nrItem
+            self.SapGuiSession.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-MATNR[0,7]").text = matnr
+            self.SapGuiSession.findById("wnd[0]/usr/sub:SAPMM07M:0421/txtMSEG-ERFMG[0,26]").text = qtdItem
+
+            self.SapGuiSession.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-LGORT[0,48]").text = stLocation
+            self.SapGuiSession.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-CHARG[0,53]").setFocus()
+            self.SapGuiSession.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-CHARG[0,53]").caretPosition = 0
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[1]/tbar[0]/btn[7]").press
+            self.SapGuiSession.findById("wnd[1]").sendVKey(7)
+            serNumber = str(self.SapGuiSession.findById("wnd[1]/usr/tblSAPLIPW1TC_SERIAL_NUMBERS/ctxtRIPW0-SERNR[0,0]").text)
+            self.SapGuiSession.findById("wnd[1]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(11)
+
+            return serNumber
+                    
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def setDefPicking(self, qtd):
+
+        try:
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nvl02n"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02").select()
+            self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/txtLIPSD-PIKMG[6,0]").text = qtd
+            self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/txtLIPSD-PIKMG[6,0]").setFocus()
+            self.SapGuiSession.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/txtLIPSD-PIKMG[6,0]").caretPosition = 1
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[1]/tbar[0]/btn[0]").press()
+
+            #Set objStdOut = WScript.StdOut
+            #objStdOut.Write session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\01/ssubSUBSCREEN_BODY:SAPMV50A:1102/tblSAPMV50ATC_LIPS_OVER/txtLIPS-POSNR[0," + str(i) + "]").text
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(11)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+
+            return
+
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def setSerialNumAndPostGI(self, serNum):
+
+        try:
+            #set serial number and post good issues
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nvl02n"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").resizeWorkingPane(128,37,False)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/mbar/menu[3]").select()
+            self.SapGuiSession.findById("wnd[0]/mbar/menu[3]/menu[3]").select()
+            self.SapGuiSession.findById("wnd[1]/usr/tblSAPLIPW1TC_SERIAL_NUMBERS/ctxtRIPW0-SERNR[0,0]").text = str(serNum)
+            self.SapGuiSession.findById("wnd[1]/usr/tblSAPLIPW1TC_SERIAL_NUMBERS/ctxtRIPW0-SERNR[0,0]").caretPosition = 3
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(11)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/tbar[1]/btn[20]").press()
+
+            return
+
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def createBilling(self):
+
+        try:
+            self.SapGuiSession.findById("wnd[0]").resizeWorkingPane(128,37,False)
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nvf01"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/btn[11]").press()
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nvf02"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+
+            return str(self.SapGuiSession.findById("wnd[0]/usr/ctxtVBRK-VBELN").text)
+
+        except:
+            
+            raise print(sys.exc_info()[0])
+
+    def getGoodsMvt(self, soNew, nrItem, tpDoc):
+
+        try:
+
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/okcd").text = "/nzse16n"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/ctxtGD-TAB").text = "VBFA"
+            self.SapGuiSession.findById("wnd[0]").sendVKey(0)
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").text = soNew
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,2]").text = nrItem
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,5]").text = tpDoc
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,5]").setFocus
+            self.SapGuiSession.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,5]").caretPosition = 1
+            self.SapGuiSession.findById("wnd[0]/tbar[1]/btn[8]").press()
+            self.SapGuiSession.findById("wnd[0]/usr/cntlRESULT_LIST/shellcont/shell").doubleClickCurrentCell()
+
+            gvmDoc = str(self.SapGuiSession.findById("wnd[1]/usr/tblSAPLTSWUSLDETAIL_TAB/txtGT_SELFIELDS-LOW[1,3]").text)
+
+            self.SapGuiSession.findById("wnd[1]").close()
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/btn[15]").press()
+            self.SapGuiSession.findById("wnd[0]/tbar[0]/btn[15]").press()
+
+            return gvmDoc
+
+        except:
+            
+            raise print(sys.exc_info()[0])
--- sapscript.py
+++ sapscript.py
@@ -1,4 +1,3 @@
-
 #import win32com.client
 import sys
 import openpyxl
@@ -14,216 +13,119 @@ def SAP_OP():
 
     pathExel = constants.FILENAME
     wb_obj = openpyxl.load_workbook(pathExel) 
-    sheet_obj = wb_obj.active 
-
-    ovOrig = sheet_obj.cell(row = 2, column = 1)  
-
+    sheet_obj = wb_obj.active
+    ovOrig = sheet_obj.cell(row = 2, column = 1) 
+    
     runScript = RunScripts(sapGui)
 
-    #script para pegar o numero da OV do
+    #Get SO from PRD
     try: 
-        ovNew = runScript.getOVfromProd(ovOrig.value)
+        ovNew = runScript.getSOfromProd(ovOrig.value)
+
     except:
         print('Falha ao recuperar doc de PRD')
         print("next document")
 
+
+    #get from spreadsheet Type SO and set SO Ref
     tpOV = sheet_obj.cell(row = 2, column = 10) 
     ovRef = sheet_obj.cell(row = 2, column = 12)
 
-    #criar OV com referencia
+    #Create SO with reference
     try:
-        sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nva01"
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]/usr/ctxtVBAK-AUART").text = tpOV.value
-        sapGui.session.findById("wnd[0]").sendVKey(8)
-        sapGui.session.findById("wnd[1]/usr/tabsMYTABSTRIP/tabpRAUF").select()
-        sapGui.session.findById("wnd[1]/usr/tabsMYTABSTRIP/tabpRAUF/ssubSUB1:SAPLV45C:0301/ctxtLV45C-VBELN").text = ovNew
-        sapGui.session.findById("wnd[1]/usr/tabsMYTABSTRIP/tabpRAUF/ssubSUB1:SAPLV45C:0301/ctxtLV45C-VBELN").caretPosition = 10
-        sapGui.session.findById("wnd[1]").sendVKey(5)
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/cmbVBAK-LIFSK").key = " "
-        sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/cmbVBAK-LIFSK").setFocus
-        sapGui.session.findById("wnd[0]/tbar[0]/btn[11]").press
-        sapGui.session.findById("wnd[0]").sendVKey(11)
-        sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nva02"
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        ovRef.value = sapGui.session.findById("wnd[0]/usr/ctxtVBAK-VBELN").text
+
+        ovRef.value = runScript.createSOByRef(tpOV.value, ovNew)
 
     except:
         print(sys.exc_info()[0])
     
-    #finally:
-        #TBD
  
-    #Quantidade de itens da OV
+    #Save Shiping point into Spredsheet
     shipPoint = sheet_obj.cell(row = 2, column = 7)
     wb_obj.save(pathExel)
 
-    sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nzse16n"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/ctxtGD-TAB").text = "vbap"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").text = str(ovRef.value)
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").setFocus()
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").caretPosition = 10
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/txtGS_SELFIELDS-FIELDNAME[6,1]").setFocus()
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/txtGS_SELFIELDS-FIELDNAME[6,1]").caretPosition = 5
-    
-    sapGui.session.findById("wnd[0]/tbar[0]/btn[71]").press()
-    sapGui.session.findById("wnd[1]/usr/sub:SAPLSPO4:0300/txtSVALD-VALUE[0,21]").text = "VSTEL"
-    sapGui.session.findById("wnd[1]/usr/sub:SAPLSPO4:0300/txtSVALD-VALUE[0,21]").caretPosition = 5
-    sapGui.session.findById("wnd[1]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,0]").text = str(shipPoint.value)
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,0]").setFocus()
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,0]").caretPosition = 4
-    sapGui.session.findById("wnd[0]/tbar[1]/btn[8]").press()
-    
-    sapGui.session.findById("wnd[0]/usr/txtGD-NUMBER").setFocus()
-    sapGui.session.findById("wnd[0]/usr/txtGD-NUMBER").caretPosition = 0
-    itmnun = sapGui.session.findById("wnd[0]/usr/txtGD-NUMBER").text
-    sapGui.session.findById("wnd[0]/tbar[0]/btn[3]").press
-    sapGui.session.findById("wnd[0]/tbar[0]/btn[3]").press 
+    #get ITM NUM by SO
+    try:
+
+        itmnun = runScript.getItmNum(ovRef.value, shipPoint.value)
+
+    except:
+        print(sys.exc_info()[0])
 
     itmnun = int(itmnun) - 1
 
-    #loop para cada item 
+    #get Stored location by ITMUN
     for i in range(int(itmnun + 1)):
 
-        #Recupera storedlocation
-        sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nvl01n"
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]/usr/ctxtLIKP-VSTEL").text = str(shipPoint.value)
-        sapGui.session.findById("wnd[0]/usr/ctxtLV50C-VBELN").text = str(ovRef.value)
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]").sendVKey(0)
+        try:
 
-        sapGui.session.findById("wnd[0]").maximize()
-        sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02").select()       
+            stLocation = sheet_obj.cell(row = 2, column = 11)
+            stLocation.value = runScript.getStoredLoc(shipPoint.value, ovRef.value, i)
 
-        print(sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/txtLIPS-POSNR[0," + str(i) +"]").text)
+        except:
+            print(sys.exc_info()[0])
 
-        #posnr =  session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\01/ssubSUBSCREEN_BODY:SAPMV50A:1102/tblSAPMV50ATC_LIPS_OVER/txtLIPS-POSNR[0," + str(i) +"]").text
-        stLocation = sheet_obj.cell(row = 2, column = 11)
-        stLocation.value =  sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/ctxtLIPS-LGORT[3," + str(i) +"]").text
 
-        
     #Delivery save 
-    sapGui.session.findById("wnd[0]").sendVKey(11)
-    sapGui.session.findById("wnd[1]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nvl02n"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    print(sapGui.session.findById("wnd[0]/usr/ctxtLIKP-VBELN").text)
-    
+    try:
+
+        deliveryValue = runScript.getDelivery()
+
+    except:
+        print(sys.exc_info()[0])
+
+
+    #Save Delivery into SpreadSheet
     dlvNum = sheet_obj.cell(row = 2, column = 13)
-    dlvNum.value = sapGui.session.findById("wnd[0]/usr/ctxtLIKP-VBELN").text
     dlvNum2 = sheet_obj.cell(row = 2, column = 14)
-    dlvNum2.value = sapGui.session.findById("wnd[0]/usr/ctxtLIKP-VBELN").text
+
+    dlvNum.value = deliveryValue
+    dlvNum2.value = deliveryValue
     wb_obj.save(pathExel)
 
+
     #define serial number/ set stock
-    sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nmb1c"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/ctxtRM07M-BWARTWA").text = "561"
-    sapGui.session.findById("wnd[0]/usr/ctxtRM07M-SOBKZ").text = "E"
-
     plant = sheet_obj.cell(row = 2, column = 3)
-    sapGui.session.findById("wnd[0]/usr/ctxtRM07M-WERKS").text = plant.value
-    sapGui.session.findById("wnd[0]/usr/ctxtRM07M-GRUND").setFocus
-    sapGui.session.findById("wnd[0]/usr/ctxtRM07M-GRUND").caretPosition = 0
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/subBLOCK1:SAPMM07M:2423/ctxtMSEGK-MAT_KDAUF").text = ovRef.value
-
     nrItem = sheet_obj.cell(row = 2, column = 6)
-    sapGui.session.findById("wnd[0]/usr/subBLOCK1:SAPMM07M:2423/txtMSEGK-MAT_KDPOS").text = nrItem.value
-
     matnr = sheet_obj.cell(row = 2, column = 2)
-    sapGui.session.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-MATNR[0,7]").text = matnr.value
-
     qtdItem = sheet_obj.cell(row = 2, column = 5)
-    sapGui.session.findById("wnd[0]/usr/sub:SAPMM07M:0421/txtMSEG-ERFMG[0,26]").text = qtdItem.value
 
-    sapGui.session.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-LGORT[0,48]").text = stLocation.value
-    sapGui.session.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-CHARG[0,53]").setFocus()
-    sapGui.session.findById("wnd[0]/usr/sub:SAPMM07M:0421/ctxtMSEG-CHARG[0,53]").caretPosition = 0
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[1]/tbar[0]/btn[7]").press
-    sapGui.session.findById("wnd[1]").sendVKey(7)
+    try:
+        serNum = sheet_obj.cell(row = 2, column = 8)
+        serNum.value = runScript.setStockAndSerNumber(ovRef.value, plant.value, nrItem.value, matnr.value, qtdItem.value, stLocation.value)
 
+    except:
+        print(sys.exc_info()[0])
+
+    
     #SAVE SERIAL NUMBER
-    serNum = sheet_obj.cell(row = 2, column = 8)
-    serNum.value = sapGui.session.findById("wnd[1]/usr/tblSAPLIPW1TC_SERIAL_NUMBERS/ctxtRIPW0-SERNR[0,0]").text
-    sapGui.session.findById("wnd[1]").sendVKey(0)
-    sapGui.session.findById("wnd[0]").sendVKey(11)
     wb_obj.save(pathExel)
 
 
     #Picking
     for i in range(int(itmnun + 1)):
-        sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nvl02n"
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02").select()
-        sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/txtLIPSD-PIKMG[6,0]").text = qtdItem.value
-        sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/txtLIPSD-PIKMG[6,0]").setFocus()
-        sapGui.session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\02/ssubSUBSCREEN_BODY:SAPMV50A:1104/tblSAPMV50ATC_LIPS_PICK/txtLIPSD-PIKMG[6,0]").caretPosition = 1
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[1]/tbar[0]/btn[0]").press()
 
-        #Set objStdOut = WScript.StdOut
-        #objStdOut.Write session.findById("wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\\01/ssubSUBSCREEN_BODY:SAPMV50A:1102/tblSAPMV50ATC_LIPS_OVER/txtLIPS-POSNR[0," + str(i) + "]").text
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]").sendVKey(11)
-        sapGui.session.findById("wnd[0]").sendVKey(0)
+        try:
+            #Picking
+            runScript.setDefPicking(qtdItem.value)
 
-        #set serial number and post good issues
-        sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nvl02n"
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]").resizeWorkingPane(128,37,False)
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]/mbar/menu[3]").select()
-        sapGui.session.findById("wnd[0]/mbar/menu[3]/menu[3]").select()
-        sapGui.session.findById("wnd[1]/usr/tblSAPLIPW1TC_SERIAL_NUMBERS/ctxtRIPW0-SERNR[0,0]").text = str(serNum.value)
-        sapGui.session.findById("wnd[1]/usr/tblSAPLIPW1TC_SERIAL_NUMBERS/ctxtRIPW0-SERNR[0,0]").caretPosition = 3
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]").sendVKey(11)
-        sapGui.session.findById("wnd[0]").sendVKey(0)
-        sapGui.session.findById("wnd[0]/tbar[1]/btn[20]").press()
+            #set serial number and post good issues
+            runScript.setSerialNumAndPostGI(serNum.value)
 
-    sapGui.session.findById("wnd[0]").resizeWorkingPane(128,37,False)
-    sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nvf01"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/tbar[0]/btn[11]").press()
+            #Set Serial number at DLV
+
+        except:
+            print(sys.exc_info()[0])
 
 
-    #recupera a billing criada
+    #get billing doc
     billDoc = sheet_obj.cell(row = 2, column = 15)
-    sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nvf02"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    billDoc.value = str(sapGui.session.findById("wnd[0]/usr/ctxtVBRK-VBELN").text)
-
+    billDoc.value = runScript.createBilling()
     wb_obj.save(pathExel)
-
 
     #GET GOODS MVT DOC
     goodMov = sheet_obj.cell(row = 2, column = 16)
-    sapGui.session.findById("wnd[0]/tbar[0]/okcd").text = "/nzse16n"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/ctxtGD-TAB").text = "VBFA"
-    sapGui.session.findById("wnd[0]").sendVKey(0)
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").text = ovNew
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,2]").text = nrItem.value
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,5]").text = constants.GOOD_MOV
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,5]").setFocus
-    sapGui.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,5]").caretPosition = 1
-    sapGui.session.findById("wnd[0]/tbar[1]/btn[8]").press()
-    sapGui.session.findById("wnd[0]/usr/cntlRESULT_LIST/shellcont/shell").doubleClickCurrentCell()
-    goodMov.value = sapGui.session.findById("wnd[1]/usr/tblSAPLTSWUSLDETAIL_TAB/txtGT_SELFIELDS-LOW[1,3]").text
-    sapGui.session.findById("wnd[1]").close()
-    sapGui.session.findById("wnd[0]/tbar[0]/btn[15]").press()
-    sapGui.session.findById("wnd[0]/tbar[0]/btn[15]").press()
-
+    goodMov.value = runScript.getGoodsMvt(ovRef.value, nrItem.value, constants.GOOD_MOV)    
     wb_obj.save(pathExel)
 
     while sapGui.connection.children.count > 0:
