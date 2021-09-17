a553e2446837450d5cf5dcf2b922f0695127545d
Rossato Euzebio <RossatoEuzebio@JohnDeere.com>
2021-09-17 17:23:47 -0300

    DSM PAG

---
+++ loginAG2.py
@@ -0,0 +1,52 @@
+import subprocess
+import time
+import win32com.client
+import psutil
+
+class SapGui(object):
+
+    def __init__(self):
+
+        for proc in psutil.process_iter():
+            if proc.name() == "saplogon.exe":
+                proc.kill()
+
+        try:
+            #verifica se o SAP GUI ja esta aberto
+            sapgui = win32com.client.GetObject("SAPGUI")
+            #sapgui.
+        except:
+            #caso não, abre via .exe
+            self.path = r"C:\Program Files (x86)\SAP\FrontEnd\SAPgui\saplogon.exe"
+            subprocess.Popen(self.path)
+            time.sleep(3)
+            
+
+        finally:
+            self.SapGuiAuto = win32com.client.GetObject("SAPGUI")
+
+        #self.SapGuiAuto = win32com.client.GetObject("SAPGUI")
+        application = self.SapGuiAuto.GetScriptingEngine
+        SapGuiAuto = None
+
+        #login AG2
+        self.connection = application.OpenConnection("AG2 - North American Ag (SSO) (003)",False)
+        time.sleep(3)
+        self.session = self.connection.Children(0)
+        self.session.findById("wnd[0]").maximize()
+
+    def killSAP(self):
+        
+        self.connection = None
+        self.session = None
+
+        for proc in psutil.process_iter():
+            if proc.name() == "saplogon.exe":
+                proc.kill()
+
+    def getSession(self):
+        return self.session
+
+
+#if __name__ == '__main__':
+#    Object  =  SapGui();
